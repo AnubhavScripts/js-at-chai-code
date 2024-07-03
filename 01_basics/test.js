447cf0a4b7c884c82a435b5745f65c2e5cb23ffd
Anubhav Parashar <154588434+AnubhavScripts@users.noreply.github.com>
2024-07-03 16:00:52 +0000

    container chanfe

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,17 @@
 console.log ("ANUBHAV");
+
+{
+"settings"; 
+{
+    "editor.fontSize"; 32;
+    "terminal.integrated.fontSize"; 24;
+    "terminal.integrated.fontSize"; 24;
+    "editor.wordWrap"; "on";
+}
+
+};
+Comment;
+
+
+
+
