ff798a2ffa9c2fb8b815c0718678016c9a177c45
Anubhav Parashar <154588434+AnubhavScripts@users.noreply.github.com>
2024-07-05 18:12:06 +0000

    f

--- 01_basics/03_conversionalOperation.js
+++ 01_basics/03_conversionalOperation.js
@@ -1,5 +1,25 @@
-let score = 33
+let score = "33abc"
 
 console.log(typeof score);
 
 let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);  // NaN stands for not a number
+
+// "33" =>  is s number 33
+// "33abc"=> is a type number but in actual it is Nan
+// true => 1 ; false => 0
+
+let isLoggedIn = "anubhav"
+
+let booleanIsLoggedIn= Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0=> false
+// emtpy space => false
+// any string => true
+
+let someNumber =33;
+let stringNumber= String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
