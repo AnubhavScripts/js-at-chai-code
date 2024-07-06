0313c44b8ed78e9f81d45043ffdd2de7dfc4418a
Anubhav Parashar <154588434+AnubhavScripts@users.noreply.github.com>
2024-07-06 19:22:17 +0000

    u

--- 01_basics/03_conversionalOperation.js
+++ 01_basics/03_conversionalOperation.js
@@ -3,8 +3,8 @@ let score = "33abc"
 console.log(typeof score);
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);  // NaN stands for not a number
+/*console.log(typeof valueInNumber);
+console.log(valueInNumber);  // NaN stands for not a number */
 
 // "33" =>  is s number 33
 // "33abc"=> is a type number but in actual it is Nan
@@ -13,7 +13,7 @@ console.log(valueInNumber);  // NaN stands for not a number
 let isLoggedIn = "anubhav"
 
 let booleanIsLoggedIn= Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 //1 => true; 0=> false
 // emtpy space => false
@@ -21,5 +21,28 @@ console.log(booleanIsLoggedIn);
 
 let someNumber =33;
 let stringNumber= String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+/*console.log(stringNumber);
+console.log(typeof stringNumber); */
+ 
+
+///*********************************. OPERATIONS****************************////////////////////////// */
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); power 
+// console.log(2/2);
+// console.log(2%2); remainder
+
+let str1= "hello";
+let str2 = " world";
+
+let str3= str1 + str2;
+/*console.log(str3);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 +"2");
+
+console.log(2+3*5%3); */
+
