1bc5ef05ad100303276359ea463c1a1fb4eb429a
Anubhav Parashar <154588434+AnubhavScripts@users.noreply.github.com>
2024-07-11 17:54:28 +0000

    hh

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,18 @@
+// #Primitive data type
+// 7 types;- boolean , number, null , undefined , string, symbol, BigInt
+
+const score = 100;
+const scoreValue = 200.33;
+const temp = null;
+const isLoggedIn= false;
+let debit;
+
+const id= Symbol('123');
+const anotherId= Symbol('123');
+console.log(id===anotherId); // the values of two variable is not same
+
+const bigNumber= 1234768746875364689n;
+
+
+// Reference data type (Non Primitive):
+// Functions, Object , Arrays
