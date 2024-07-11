0a567299b6a426456d857bf12a185a99b77d01af
Anubhav Parashar <154588434+AnubhavScripts@users.noreply.github.com>
2024-07-11 18:28:25 +0000

    update

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -16,3 +16,23 @@ const bigNumber= 1234768746875364689n;
 
 // Reference data type (Non Primitive):
 // Functions, Object , Arrays
+const heroes = ["shaktiman", "naagraj", "hanuman"] ;// array
+let MyObj= {
+    name: "anubhav",
+    age: 19,
+}
+
+const myFunction = function () {
+    console.log( "hello world");
+    
+}
+
+console.log(typeof heroes);// object
+console.log(typeof myFunction);// function
+console.log(typeof MyObj);// object
+console.log(typeof bigNumber);// bigInt
+console.log(typeof temp);//null ==> object
+console.log(typeof id);// symbol
+console.log(typeof debit);// undefined
+console.log(typeof scoreValue);// number
+console.log(typeof temp);// objeCt
