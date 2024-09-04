baa7a449792b11f564183d18db1b245abfd2dfff
Anubhav <kanubhav998@gmail.com>
2024-09-05 01:05:02 +0530

    32

---
+++ 03_basics.js/01_function.js
@@ -0,0 +1,26 @@
+
+function sayMyName(){
+console.log("e");
+console.log("x");
+console.log("o");
+console.log("n");
+console.log("o");
+console.log("v");
+console.log("a");
+ 
+}
+sayMyName()
+
+// function addTwoNumbers(number1,number2){
+//     console.log(number1 + number2);
+    
+// }
+
+function addTwoNumbers (number1,number2){
+//   let result = number1 + number2  
+//   return result
+return number1 + number2
+        
+     } 
+addTwoNumbers(1,2)//3 is output
+console.log("result:", result);
