a5a66fa81b00265c2a1e28b6b7944aa5fbdf1e88
Anubhav <kanubhav998@gmail.com>
2024-09-08 10:03:10 +0530

    almost completed functiom

--- 03_basics.js/01_function.js
+++ 03_basics.js/01_function.js
@@ -18,9 +18,25 @@ sayMyName()
 
 function addTwoNumbers (number1,number2){
 //   let result = number1 + number2  
-//   return result
+//   return result  ##agar ye end mei hai toh iske baad kuch print nhi hoga
 return number1 + number2
         
      } 
 addTwoNumbers(1,2)//3 is output
-console.log("result:", result);
+// console.log("result:", result); 
+
+function loginUserMessage(username){
+     if (username === undefined) {
+          console.log("Please enter username");
+          return
+
+     }
+     return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("anubhav"));
+console.log(loginUserMessage());// agar koi bhi value put nhi kiya and tb agar 
+//print krte hai toh #undefined aata hai @null nhi aata 
+
+
+
