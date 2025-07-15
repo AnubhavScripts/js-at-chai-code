6c5f86834812fd18d9c59ade368f68c4958ca02e
Anubhav <kanubhav998@gmail.com>
2025-07-15 23:49:23 +0530

    revision

---
+++ 05_iteration.js/two.js
@@ -0,0 +1,22 @@
+// let index =0
+
+// while (index<= 10 ) {
+//     console.log(`value of i ${index}`);
+//     index = index +2;
+//  }
+
+//  let myarray= ["shaktiman", " papa", "maa","bhagwanji"]
+// let arr= 0;
+// while (arr<myarray.length){
+//     console.log(`value is ${myarray[arr]}`);
+//     arr= arr+1
+    
+// }
+
+let score =1
+do {
+    console.log(`score is ${score}`);
+    score++;
+    
+    
+} while (score<=10);
